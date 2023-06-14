d58d388d548e7bc54b2f6f27fa4dd48cfd43a6aa
stylish-pawmy <ambesi2026@gmail.com>
2023-06-14 21:47:13 +0100

    Introduce Category model

---
+++ Shared/Category.cs
@@ -0,0 +1,8 @@
+namespace BlazorEcommerce.Shared;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+}
