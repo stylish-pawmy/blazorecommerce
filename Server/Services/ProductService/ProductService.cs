98afd15333435bb4fb99da037063dbee1076116c
stylish-pawmy <ambesi2026@gmail.com>
2023-06-15 06:59:08 +0100

    Include Product Variants in Product Service fetch methods

--- Server/Services/ProductService/ProductService.cs
+++ Server/Services/ProductService/ProductService.cs
@@ -13,7 +13,10 @@ public class ProductService : IProductService
     public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
     {
         var response = new ServiceResponse<List<Product>> {
-            Data = await _context.Products.ToListAsync()
+            Data = await _context.Products
+            .Include(p => p.Variants)
+            .ThenInclude(v => v.ProductType)
+            .ToListAsync()
         };
         
         return response;
@@ -23,6 +26,8 @@ public class ProductService : IProductService
     {
         var response = new ServiceResponse<List<Product>> {
             Data = await _context.Products
+            .Include(p => p.Variants)
+            .ThenInclude(v => v.ProductType)
             .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
             .ToListAsync()
         };
@@ -33,7 +38,10 @@ public class ProductService : IProductService
     public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
     {
         var response = new ServiceResponse<Product>();
-        var product = await _context.Products.FindAsync(productId);
+        var product = await _context.Products
+        .Include(p => p.Variants)
+        .ThenInclude(v => v.ProductType)
+        .FirstOrDefaultAsync(p => p.Id == productId);
 
         if (product is null)
         {
