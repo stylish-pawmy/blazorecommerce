50281af77366b819be0eda24ff734634471d4e84
stylish-pawmy <ambesi2026@gmail.com>
2023-06-15 05:10:15 +0100

    Implement product list state change according to categories

--- Client/Services/ProductService/IProductService.cs
+++ Client/Services/ProductService/IProductService.cs
@@ -2,7 +2,7 @@ namespace BlazorEcommerce.Client.Services.ProductService;
 
 public interface IProductService
 {
+    public event Action ProductListChanged;
     public List<Product> Products { get; set; }
-
-    public Task GetProducts();
+    public Task GetProducts(string? categoryUrl = null);
 }
--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -3,19 +3,23 @@ namespace BlazorEcommerce.Client.Services.ProductService;
 public class ProductService : IProductService
 {
     private readonly HttpClient _http;
+    public event Action ProductListChanged = () => {};
+    public List<Product> Products { get; set; } = new List<Product>();
 
     public ProductService(HttpClient http)
     {
         _http = http;
     }
-    public List<Product> Products { get; set; } = new List<Product>();
 
-    public async Task GetProducts()
+    public async Task GetProducts(string? categoryUrl = null)
     {
-        var response =
-            await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+        var response = categoryUrl == null ?
+            await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product"):
+            await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
         
         if (response is not null && response.Data is not null)
             Products = response.Data;
+        
+        ProductListChanged.Invoke();
     }
 }
