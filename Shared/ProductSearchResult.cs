dfdeeda390fd8c99588725edc01f7e6e631dceda
stylish-pawmy <ambesi2026@gmail.com>
2023-06-17 06:26:02 +0100

    Add product search result dto model

---
+++ Shared/ProductSearchResult.cs
@@ -0,0 +1,8 @@
+namespace BlazorEcommerce.Shared;
+
+public class ProductSearchResult
+{
+    public List<Product> Products { get; set; } = new List<Product>();
+    public int Pages { get; set; }
+    public int CurrentPage { get; set; }
+}
