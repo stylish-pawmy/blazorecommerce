d8ce7a5741df94c528e4ca67ffe586b22718f108
stylish-pawmy <ambesi2026@gmail.com>
2023-06-18 09:31:18 +0100

    Add user registration model

---
+++ Shared/UserRegister.cs
@@ -0,0 +1,8 @@
+namespace BlazorEcommerce.Shared;
+
+public class UserRegister
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
