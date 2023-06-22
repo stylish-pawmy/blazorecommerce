40b6c7729c96308c47a9a52d279dd8732d15959d
stylish-pawmy <ambesi2026@gmail.com>
2023-06-22 06:54:06 +0100

    Add data annotations and data validations for the register page

--- Shared/UserRegister.cs
+++ Shared/UserRegister.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorEcommerce.Shared;
 
 public class UserRegister
 {
+    [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
+    [Required, StringLength(100, MinimumLength = 6)]
     public string Password { get; set; } = string.Empty;
+    [Compare("Password", ErrorMessage = "The two passwords do not match.")]
     public string ConfirmPassword { get; set; } = string.Empty;
 }
