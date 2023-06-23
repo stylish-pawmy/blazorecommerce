8c71f7e422743cbc9daf9261d7e37cdc7cc3cfde
stylish-pawmy <ambesi2026@gmail.com>
2023-06-23 09:07:11 +0100

    Add profile page

--- Server/Services/AuthService/AuthService.cs
+++ Server/Services/AuthService/AuthService.cs
@@ -89,7 +89,7 @@ public class AuthService : IAuthService
     {
         List<Claim> claims = new List<Claim> {
             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-            new Claim(ClaimTypes.Email, user.Email)
+            new Claim(ClaimTypes.Name, user.Email),
         };
 
         var key = new SymmetricSecurityKey(
